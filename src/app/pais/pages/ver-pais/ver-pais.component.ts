e20dc225679d1ab4bdd7a89b7e8c59151102a3a0
Alixson Bastidas <alixsonbastidas@gmail.com>
2022-05-25 13:26:43 -0300

    App para buscar información sobre todos los paises del mundo

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
+
+  constructor( 
+    private route: ActivatedRoute,
+    private paisService: PaisService
+    ) { }
+
+  ngOnInit(): void {
+
+    this.route.params
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorCodigo( id ) ),
+        tap( console.log )
+        )
+        .subscribe( pais => this.pais = pais );
+
+      
+
+    // this.route.params
+    //   .subscribe ( ({ id }) => {
+    //     console.log( id )
+
+    //     this.paisService.getPaisPorCodigo( id )
+    //     .subscribe( pais => {
+    //       console.log(pais);
+    //     } )
+    //   } )
+
+  }
+
+}
